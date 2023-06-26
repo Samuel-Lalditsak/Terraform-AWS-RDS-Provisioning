e5589fb4e6a67d9c48c81b17309270d60375306d
Samuel Lalditsak Tuolor <137388781+Samuel-Lalditsak@users.noreply.github.com>
2023-06-26 16:00:02 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,16 +1,12 @@
 resource "aws_security_group" "team3-db" {
-  name        = "team3-db"
-  description = "Allow Mysql traffic"
+  name        = "team3-db-security-group"
+  description = "Security group for team3-db"
   vpc_id      = module.vpc.vpc_id
   ingress {
-    description = "Allow mysql"
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0",
-      "127.0.0.1/32",
-    ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
