129d549edb04a180ca4a6e1151017d99427bf0ba
Samuel Lalditsak Tuolor <137388781+Samuel-Lalditsak@users.noreply.github.com>
2023-06-26 15:55:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 resource "random_password" "password" {
-  length           = 30
-  special          = false
-  override_special = "_%@"
+  length           = 30           # Length of the generated password
+  special          = false        # Whether to include special characters
+  override_special = "_%@"        # Special characters to include if 'special' is false
 }
 
 resource "aws_ssm_parameter" "team3db" {
-  name  = var.aws_ssm_parameter_name
-  type  = "SecureString"
-  value = random_password.password.result
+  name  = var.aws_ssm_parameter_name        # Name of the SSM parameter
+  type  = "SecureString"                    # Type of the SSM parameter
+  value = random_password.password.result   # Value of the SSM parameter set to the generated random password
 }
 
 resource "aws_db_subnet_group" "default" {
-  name        = "example-db-subnet-group"
-  description = "Example DB subnet group"
-  subnet_ids  = ["subnet-12345678", "subnet-23456789"]  # Replace with actual subnet IDs
+  name        = "example-db-subnet-group"                  # Name of the DB subnet group
+  description = "Example DB subnet group"                  # Description of the DB subnet group
+  subnet_ids  = ["subnet-12345678", "subnet-23456789"]     # Replace with actual subnet IDs
 }
 
 resource "aws_rds_cluster" "default" {
-  cluster_identifier   = "dbname"
-  engine               = "aurora"
-  engine_version       = "5.6.10a"
-  database_name        = "dbname"
-  master_username      = "team3db"
-  master_password      = random_password.password.result
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  skip_final_snapshot  = true
+  cluster_identifier   = "dbname"      # Identifier for the RDS cluster
+  engine               = "aurora"      # Database engine for the RDS cluster
+  engine_version       = "5.6.10a"     # Version of the database engine
+  database_name        = "dbname"      # Name of the database
+  master_username      = "team3db"     # Master username for the RDS cluster
+  master_password      = random_password.password.result      # Master password set to the generated random password
+  db_subnet_group_name = aws_db_subnet_group.default.name     # Name of the DB subnet group associated with the cluster
+  skip_final_snapshot  = true          # Whether to skip the final snapshot when deleting the cluster
   vpc_security_group_ids = [
-    aws_security_group.team3-db.id
+    aws_security_group.team3-db.id     # ID of the security group associated with the cluster (Not provided in the given file)
   ]
 }
 
