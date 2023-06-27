82f76c0a43a10729511ee6e3cdf5b2a047bc90eb
Samuel Lalditsak Tuolor <137388781+Samuel-Lalditsak@users.noreply.github.com>
2023-06-27 12:16:30 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "team3-db" {
-  name        = "team3-db-security-group"
-  description = "Security group for team3-db"
-  vpc_id      = module.vpc.vpc_id
+  name        = "team3-db-security-group"          # Name of the security group
+  description = "Security group for team3-db"      # Description of the security group
+  vpc_id      = module.vpc.vpc_id                  # ID of the VPC where the security group will be created
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 3306              # Port range start for inbound traffic
+    to_port     = 3306              # Port range end for inbound traffic
+    protocol    = "tcp"             # Protocol for inbound traffic
+    cidr_blocks = ["0.0.0.0/0"]     # CIDR blocks to allow inbound traffic from (in this case, allowing from any IP)
   }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0              # Port range start for outbound traffic
+    to_port     = 0              # Port range end for outbound traffic
+    protocol    = "-1"           # Protocol for outbound traffic (-1 represents all protocols)
+    cidr_blocks = ["0.0.0.0/0"]  # CIDR blocks to allow outbound traffic to (in this case, allowing to any IP)
   }
 }
