ca0628c53cff2c5a6645e9b661840a2d8235003a
Samuel Lalditsak Tuolor <137388781+Samuel-Lalditsak@users.noreply.github.com>
2023-06-27 13:22:10 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,3 @@
 variable "aws_ssm_parameter_name" {
-    default = "/devops/ap-south-1/passwords/team3db-cluster"
+    default = "/devops/ap-south-1/passwords/team3db-cluster"        # Default value for the AWS SSM parameter name variable
 }
