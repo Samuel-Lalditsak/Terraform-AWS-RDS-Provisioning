c0f7e8cb41cf6c1b61154f8d9e802a19fc968f96
Samuel Lalditsak Tuolor <137388781+Samuel-Lalditsak@users.noreply.github.com>
2023-06-27 12:30:12 +0530

    Update subnet_group.tf

--- subnet_group.tf
+++ subnet_group.tf
@@ -1,8 +1,8 @@
 resource "aws_db_subnet_group" "default" {
-  name       = "main"
+  name       = "main"                # Name of the DB subnet group
   subnet_ids = [
-    module.vpc.private_subnets[0],
-    module.vpc.private_subnets[1],
-    module.vpc.private_subnets[2],
+    module.vpc.private_subnets[0],   # First private subnet ID from the VPC module
+    module.vpc.private_subnets[1],   # Second private subnet ID from the VPC module
+    module.vpc.private_subnets[2],   # Third private subnet ID from the VPC module
   ]
 }
